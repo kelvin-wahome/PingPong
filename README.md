b97d86ae389f243eb2b8749dfe090fed7adbbe45
kelvin-wahome <wahomekelving@gmail.com>
2017-06-16 11:45:01 +0300

    returned pingponged

---
+++ README.md
@@ -0,0 +1,7 @@
+#project:PingPong
+##creator:kelvinwahome
+### the user should be able to key in any number that:
+
+Numbers divisible by 3 are replaced with "ping"
+Numbers divisible by 5 are replaced with "pong"
+Numbers divisible by 15 are replaced with "pingpong
--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@ function pingPong(userInput){
   var pingponged = [];
 
   for (var fx = 1; fx <= userInput; fx+=1){
-    if (fx % 15) === 0){
+    if (fx % 15 === 0){
       pingponged.push ("Ping-Pong");
     }
-     else if (fx % 5) === 0){
+     else if (fx % 5 === 0){
       pingponged.push ("Pong");
     }
-     else if (fx % 3) === 0){
+     else if (fx % 3 === 0){
       pingponged.push ("Ping");
     }
      else{
     pingponged.push (fx);
 
         }
-    }
+    } return pingponged
 
 }
 
@@ -27,13 +27,13 @@ function pingPong(userInput){
 
 
 $(document).ready(function() {
-  $("form#pingPong").submit(function(event) {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
     $("#userOutput").empty();
     var userInput = parseInt($("input#number").val());
     var result = pingPong(userInput);
-    pingponged.forEach(function(output) {
+    result.forEach(function(output) {
     $("#userOutput").append("<li>" + output + "</li>");
-    event.preventDefault();
   });
 });
 });
